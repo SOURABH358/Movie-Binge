747a9e832b9f32d2371e5cf2976f267682a36d74
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-09-24 10:37:19 +0530

    modified styling

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 const sliderRight = document.querySelector('.right-slider') 
 const sliderLeft = document.querySelector('.left-slider')
 const poster = document.querySelectorAll('.movie-poster div')
-const moviePoster = document.querySelector('.movie-poster')
+const root = document.querySelector(':root')
 
 document.addEventListener("DOMContentLoaded",()=>{
     poster[1].className = "show"
@@ -11,37 +11,36 @@ const len = poster.length
 
 let i = 1;
 let j = 1;
-const arr = ['toRight','mid','toLeft']
+let val = 0;
 // console.log(len)
 sliderLeft.addEventListener('click',()=>{
-    if(i>0){
-        Array.from(poster).map(ele=>{
-            ele.className = 'hide'
-        })
-        i-=1
-        poster[i].className = ''
-        moviePoster.setAttribute("translateX","(-33%)")
-        if(j>0){
-
-            j-=1;
-            moviePoster.className = 'movie-poster'
-            moviePoster.classList.add(arr[j])
-        }
-    }    
-    // console.log(poster.children[i])
-})
-sliderRight.addEventListener('click',()=>{
     if(i<len-1){
         Array.from(poster).map(ele=>{
             ele.className = 'hide'
         })
         i+=1
+        poster[i].className = ''
+        if(j>0){
+
+            j-=1;
+            val-=33;
+            root.style.setProperty('--i',`${val}`)   
+        }
+    }    
+    // console.log(poster.children[i])
+})
+sliderRight.addEventListener('click',()=>{
+    if(i>0){
+        Array.from(poster).map(ele=>{
+            ele.className = 'hide'
+        })
+        i-=1
         poster[i].className = 'show'
         if(j<2){
     
             j+=1;
-            moviePoster.className = 'movie-poster'
-            moviePoster.classList.add(arr[j])
+            val+=33;
+            root.style.setProperty('--i',val)
         }
     }    
     // console.log(poster.children[i])
