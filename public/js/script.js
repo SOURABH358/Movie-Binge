78fabe6f51d00694e7999dfbf4d945593557179c
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-09-24 10:03:46 +0530

    modified styling

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,49 @@
 const sliderRight = document.querySelector('.right-slider') 
 const sliderLeft = document.querySelector('.left-slider')
-let poster = document.querySelector('.movie-poster div')
+const poster = document.querySelectorAll('.movie-poster div')
+const moviePoster = document.querySelector('.movie-poster')
 
+document.addEventListener("DOMContentLoaded",()=>{
+    poster[1].className = "show"
 
-const len = poster.children.length
+})
+const len = poster.length
 
-let i = 0;
+let i = 1;
+let j = 1;
+const arr = ['toRight','mid','toLeft']
 // console.log(len)
 sliderLeft.addEventListener('click',()=>{
     if(i>0){
-        Array.from(poster.children).map(ele=>{
-            ele.classList.add('hide')
+        Array.from(poster).map(ele=>{
+            ele.className = 'hide'
         })
         i-=1
-        poster.children[i].className = ''
+        poster[i].className = ''
+        moviePoster.setAttribute("translateX","(-33%)")
+        if(j>0){
 
+            j-=1;
+            moviePoster.className = 'movie-poster'
+            moviePoster.classList.add(arr[j])
+        }
     }    
     // console.log(poster.children[i])
 })
 sliderRight.addEventListener('click',()=>{
     if(i<len-1){
-        Array.from(poster.children).map(ele=>{
+        Array.from(poster).map(ele=>{
             ele.className = 'hide'
         })
         i+=1
-        poster.children[i].className = 'show'
+        poster[i].className = 'show'
+        if(j<2){
+    
+            j+=1;
+            moviePoster.className = 'movie-poster'
+            moviePoster.classList.add(arr[j])
+        }
     }    
     // console.log(poster.children[i])
-
 
 })
